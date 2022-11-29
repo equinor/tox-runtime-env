b1ad55b26280abaefa1593c3835f5bb824622a8f
mferrera <matt.ferrera@gmail.com>
2022-11-29 15:29:04 +0100

    lint: minor setup() fixup

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ def local_scheme(version):
     return ""
 
 
-setup(use_scm_version={
-    "local_scheme": local_scheme,
-    "write_to": "src/tox_runtime_env/version.py",
-})
+setup(
+    use_scm_version={
+        "local_scheme": local_scheme,
+        "write_to": "src/tox_runtime_env/version.py",
+    }
+)
